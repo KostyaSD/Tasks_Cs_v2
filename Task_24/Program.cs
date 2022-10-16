ff653ca3f0a37bb388a3287d4bd5f66f21e8372d
Konstantsin <Drakula3D@mail.ru>
2022-10-16 19:05:11 +0300

    add Task_24, Task_25

---
+++ Task_24/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_24
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*Задача 24: Напишите программу, которая принимает на вход число (А)
+             и выдаёт сумму чисел от 1 до А.
+             7 -> 28
+             4 -> 10
+             8 -> 36*/
+            
+            Console.Write("Введите число А: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            if (num > 1)
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    sum += i;
+                }
+                Console.Write($"Ответ: Сумма чисел от 1 до {num} = {sum}");
+            }
+            else Console.Write("Ответ: Прочитайте внимательно условие \"от 1\", это значит первая цифра 2!");
+        }
+    }
+}
