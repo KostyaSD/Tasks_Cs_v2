090f26131cc1a28ebca3b76ab858e6cc2aa7c441
Konstantsin <Drakula3D@mail.ru>
2022-10-16 18:24:53 +0300

    add Task_14

---
+++ Task_14/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_14
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*Задача 14: Напишите программу, которая принимает на вход число и проверяет,
+             кратно ли оно одновременно 7 и 23.
+
+             14 -> Нет
+             46 -> Нет
+             161 -> Да*/
+            
+            Console.Write("Введите число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 7 == 0 && num % 23 == 0) Console.WriteLine($"Ответ: Да, число {num} одновременно " +
+                                                                   "кратно 7 и 23.");
+            else Console.WriteLine($"Ответ: Нет, число {num} одновременно не кратно 7 и 23.");
+        }
+    }
+}
