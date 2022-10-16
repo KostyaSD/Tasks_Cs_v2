63ce19e131207b0b6c4b0a1b5fdd68d768645fd8
Konstantsin <Drakula3D@mail.ru>
2022-10-16 21:08:49 +0300

    add Task_35

--- Task_35/Program.cs
+++ Task_35/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Task_35
+﻿using System;
+
+namespace Task_35
 {
     internal class Program
     {
@@ -11,7 +13,41 @@
                          [5, 18, 123, 6, 2] -> 1
                          [1, 2, 3, 6, 2] -> 0
                          [10, 11, 12, 13, 14] -> 5*/
-            
+
+            int[] arr = new int[123];
+
+            RandomArr(arr);
+            PrintArr(arr);
+            CounArr(arr);
+        }
+
+        private static void CounArr(int[] arr)
+        {
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] >= 10 && arr[i] < 100) count++;
+            }
+            Console.WriteLine($"Количество эллементов: {count}");
+        }
+
+        private static void PrintArr(int[] arr)
+        {
+            Console.Write("Массив: {");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i == arr.Length - 1) Console.WriteLine(arr[i] + "}");
+                else Console.Write($"{arr[i]}, ");
+            }
+        }
+
+        private static void RandomArr(int[] arr)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(-1000, 1001);
+            }
         }
     }
 }
