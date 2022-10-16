574ab8c72fe5137391a14378537ce76b6e29e62b
Konstantsin <Drakula3D@mail.ru>
2022-10-16 19:36:37 +0300

    add Task_30

---
+++ Task_30/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_30
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*Задача 30: Напишите программу, которая выводит массив из 8 элементов,
+             заполняющий нулями и единицами в случайном порядке.
+
+             {1,0,1,1,0,1,0,0}*/
+            
+            Random rand = new Random();
+            int[] arr = new int[8];
+            Console.Write("Ответ: {");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(0, 2);
+                if (i == arr.Length - 1) Console.Write(arr[i] + "}");
+                else Console.Write($"{arr[i]}, ");
+            }
+        }
+    }
+}
