0b2000e75ac834764d80018f7a619b7bd77fbd8d
Konstantsin <Drakula3D@mail.ru>
2022-10-16 18:59:55 +0300

    add Task_21, Task_22

---
+++ Task_21/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task_21
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*Задача 21: Напишите программу, которая принимает на вход координаты двух точек 
+             и находит расстояние между ними в 3D пространстве.
+
+             A (3,6,8); B (2,1,-7), -> 15.84
+             A (7,-5, 0); B (1,-1,9) -> 11.53*/
+            
+            Console.Write("Введите координату x1: ");
+            int numXa = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите координату y1: ");
+            int numYa = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите координату z1: ");
+            int numZa = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите координату x2: ");
+            int numXb = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите координату y2: ");
+            int numYb = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите координату z2: ");
+            int numZb = Convert.ToInt32(Console.ReadLine());
+            double d = Math.Pow((Math.Pow(numXb - numXa, 2) +
+                                 Math.Pow(numYb - numYa, 2) +
+                                 Math.Pow(numZb - numZa, 2)), 0.5);
+            d = Math.Round(d, 2);
+            Console.WriteLine("Ответ: Расстояние между точками в 3D пространстве = " + d);
+        }
+    }
+}
---
+++ Task_22/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task_22
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*Задача 22: Напишите программу, которая принимает на вход число (N)
+             и выдаёт таблицу квадратов чисел от 1 до N.
+
+             5 -> 1, 4, 9, 16, 25.
+             2 -> 1, 4.*/
+            
+            Console.Write("Введите число N: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num > 0)
+            {
+                Console.WriteLine($"Ответ: Таблица кубов чисел от 1 до {num}:");
+                for (int i = 1; i <= num; i++)
+                {
+                    Console.WriteLine($"{i}^2 = {Math.Pow(i, 2)} ");
+                }
+            }
+            else Console.WriteLine($"Ответ: Число {num} не попало в интервал от 1");
+        }
+    }
+}
