acfd0cf31f4b9f47364402fddd24706c4197f1db
Konstantsin <Drakula3D@mail.ru>
2022-10-16 19:10:00 +0300

    add Task_28

---
+++ Task_28/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_28
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*Задача 28: Напишите программу, которая принимает на вход число N и выдает
+             произведение чисел от 1 до N.
+
+             4 -> 24
+             5 -> 120*/
+            
+            Console.Write("Введите число N: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int count = 1;
+            if (num > 1)
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    count *= i;
+                }
+                Console.Write($"Ответ: Произведение чисел от 1 до {num} = {count}");
+            }
+            else Console.Write("Ответ: Прочитайте внимательно условие \"от 1\", это значит первая цифра 2!");
+        }
+    }
+}
