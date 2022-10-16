229936fe7e9f534ea8d4ea33f22913fd71b4dbb8
Konstantsin <Drakula3D@mail.ru>
2022-10-16 18:41:01 +0300

    add Task_18

---
+++ Task_18/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task_18
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*Задача 18: Напишите программу, которая по заданному номеру четверти,
+             показывает диапазон возможных координат точек в этой четверти (х, y).
+
+             1 -> (x > 0, y > 0)
+             2 -> (x < 0, y > 0)*/
+            
+            Console.Write("Введите номер четверти: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    Console.Write("Ответ: Точка находится в диапозоне (x > 0, y > 0).");
+                    break;
+                case 2:
+                    Console.Write("Ответ: Точка находится в диапозоне (x < 0, y > 0).");
+                    break;
+                case 3:
+                    Console.Write("Ответ: Точка находится в диапозоне (x < 0, y < 0).");
+                    break;
+                case 4:
+                    Console.Write("Ответ: Точка находится в диапозоне (x > 0, y < 0).");
+                    break;
+                default:
+                    Console.Write("Ответ: К сожалению в нашем случае четверти бывают от 1 до 4.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Task_19/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Task_19
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
