2942dcb48797875fa5542bf5685987f7f55f4372
Konstantsin <Drakula3D@mail.ru>
2022-10-16 20:55:31 +0300

    add Task_33

--- Task_33/Program.cs
+++ Task_33/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Task_33
+﻿using System;
+
+namespace Task_33
 {
     internal class Program
     {
@@ -9,7 +11,25 @@
                          
                          4; массив [6, 7, 19, 345, 3] -> нет
                          -3; массив [6, 7, 19, 345, 3] -> да*/
-            
+
+            int[] arr = { 6, 7, 19, 345, 3 };
+
+            Console.Write("Введите число: ");
+            int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+            SearNumber(num, arr);
+
+        }
+
+        private static void SearNumber(int num, int[] arr)
+        {
+            int result = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == num) result = 1;
+            }
+            if (result == 1) Console.WriteLine("Да");
+            else Console.WriteLine("Нет");
         }
     }
 }
