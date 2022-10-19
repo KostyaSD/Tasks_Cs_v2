c6c3b5da7676e912b66ceb0a114300941f171b82
Konstantsin <Drakula3D@mail.ru>
2022-10-19 20:17:02 +0300

    add Task_45

--- Task_45/Program.cs
+++ Task_45/Program.cs
@@ -8,82 +8,79 @@ namespace Task_45
         {
             /*Задача 45: Напишите программу, которая будет создавать копию заданного
              массива с помощью поэлементного копирования*/
-            
-            int[] array1 = new Int32[8];
-            int[] array2 = new Int32[8];
-            
+
+            int[] array1 = new int[8];
+            int[] array2 = new int[8];
+
             randomFilling(array1, 0, 100);
-            copy(array1,array2);
-            arrayOutput(array1);
-            arrayOutput(array2);
-            
+            copy(array1, array2);
+            PrintArr(array1);
+            PrintArr(array2);
+
             void copy(int[] arr, int[] arr2)
             {
                 for (int i = 0; i < arr.Length; i++)
                 {
-                    arr[i] = arr2[i];
+                    arr2[i] = arr[i];
                 }
             }
-            
-            void randomFilling(int[] arr, int min, int max)
-            {
-                for (int i = 0; i < arr.Length; i++)
-                {
-                    arr[i] = new Random().Next(min, max);
-                }
-            }
-
-            void arrayOutput(int[] arr)
-            {
-                for (int i = 0; i < arr.Length; i++)
-                {
-                    Console.Write($"{arr[i]}");
-                    if (i < (arr.Length-1)) Console.Write($", ");
-        
-                }
-                Console.WriteLine(" ");
-            }
-            
-            /*
-            int[] array1 = new Int32[8];
-            int[] array2 = new Int32[8];
-
-            randomFilling(array1, 0, 100);
-            arrayOutput(array1);
-            copy(array1, array2);
-            arrayOutput(array2);
-
-            void copy(int[] array1, int[] array2)
-            {
-                for (int i = 0; i < array1.Length; i++)
-                {
-                    array2[i] = array1[i];
-                }
-            }
-
 
             void randomFilling(int[] arr, int min, int max)
             {
+                Random random = new Random();
                 for (int i = 0; i < arr.Length; i++)
                 {
-                    arr[i] = new Random().Next(min, max);
+                    arr[i] = random.Next(min, max);
                 }
             }
 
-            void arrayOutput(int[] arr)
+        }
+        private static void PrintArr(int[] arr)
+        {
+            Console.Write("Массив: {");
+            for (int i = 0; i < arr.Length; i++)
             {
-                for (int i = 0; i < arr.Length; i++)
-                {
-                    Console.Write($"{arr[i]}");
-                    if (i < (arr.Length-1)) Console.Write($", ");
-        
-                }
-                Console.WriteLine(" ");
+                if(i != arr.Length - 1) Console.Write($"{arr[i]}, ");
+                else Console.WriteLine(arr[i] + "}");
             }
-            */
+        }
 
-            
+        /*
+        int[] array1 = new Int32[8];
+        int[] array2 = new Int32[8];
+
+        randomFilling(array1, 0, 100);
+        arrayOutput(array1);
+        copy(array1, array2);
+        arrayOutput(array2);
+
+        void copy(int[] array1, int[] array2)
+        {
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array2[i] = array1[i];
             }
+        }
 
+
+        void randomFilling(int[] arr, int min, int max)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new Random().Next(min, max);
+            }
+        }
+
+        void arrayOutput(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]}");
+                if (i < (arr.Length-1)) Console.Write($", ");
+    
+            }
+            Console.WriteLine(" ");
+        }
+        */
         }
 }
