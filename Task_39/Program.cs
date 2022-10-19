af882166fbcc72c481d77550c731a6bde7fb5689
Konstantsin <Drakula3D@mail.ru>
2022-10-19 19:58:01 +0300

    add Task_39

--- Task_39/Program.cs
+++ Task_39/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Task_39
+﻿using System;
+using System.Collections;
+
+namespace Task_39
 {
     internal class Program
     {
@@ -9,7 +12,44 @@
                          
                          [1 2 3 4 5] -> [5 4 3 2 1]
                          [6 7 3 6] -> [6 3 7 6]*/
-    
+            
+            Console.Write("Введите длину массива: ");
+            int countArr = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[countArr];
+            RandomArray(arr, -20, 21); // заполнение массива от [-20; 21).
+            PrintArr(arr); // вывод массива
+            ReversArr(arr); //разворот массива
+            PrintArr(arr); // вывод массива
+        }
+
+        private static void ReversArr(int[] arr)
+        {
+            int count = 0;
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                count = arr[i];
+                arr[i] = arr[arr.Length - 1 - i];
+                arr[arr.Length - i - 1] = count;
+            }
+        }
+
+        private static void PrintArr(int[] arr)
+        {
+            Console.Write("Массив: {");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(i != arr.Length - 1) Console.Write($"{arr[i]}, ");
+                else Console.WriteLine(arr[i] + "}");
+            }
+        }
+
+        private static void RandomArray(int[] arr, int min, int max)
+        {
+            Random random = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(min, max);
+            }
         }
     }
 }
