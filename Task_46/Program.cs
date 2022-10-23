b691261449e5fb347a8aa100b9ece1c7d41d4957
Konstantsin <Drakula3D@mail.ru>
2022-10-23 14:39:04 +0300

    add Task_46

--- Task_46/Program.cs
+++ Task_46/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Task_46
+﻿using System;
+
+namespace Task_46
 {
     internal class Program
     {
@@ -11,6 +13,42 @@
                          1 4 8 19
                          5 -2 33 -2
                          77 3 8 1*/
+
+            //int numArr = Convert.ToInt32(Console.ReadLine());
+            int numArrM = 3;
+            int numArrN = 4;
+
+            int[,] arr = new int[numArrM, numArrN];
+
+            FillArray(arr);
+            PrintArray(arr);
+        }
+
+        private static void PrintArray(int[,] arr)
+        {
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i, j] < 10 && arr[i, j] >= 0) Console.Write($"  {arr[i, j]} ");
+                    else if (arr[i, j] < -9 || arr[i, j] > 99) Console.Write($"{arr[i, j]} ");
+                    else Console.Write($" {arr[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void FillArray(int[,] arr)
+        {
+            Random random = new Random();
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = random.Next(-20, 120);
+                }
+            }
         }
     }
 }
