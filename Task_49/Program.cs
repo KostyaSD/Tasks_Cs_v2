5273e5337898dd02df854acc3a93c18043c46845
Konstantsin <Drakula3D@mail.ru>
2022-10-23 14:58:52 +0300

    add Task_49

--- Task_49/Program.cs
+++ Task_49/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Task_49
+﻿using System;
+
+namespace Task_49
 {
     internal class Program
     {
@@ -13,6 +15,56 @@
                          5 9 2 3                                5 9 2 3
                          8 4 2 4                                64 4 4 4 */
             
+            int numArrM = 5;
+            int numArrN = 4;
+
+            int[,] arr = new int[numArrM, numArrN];
+
+            FillArray(arr);
+            PrintArray(arr);
+            EvenIndexArr(arr);
+            PrintArray(arr);
+        }
+
+        private static void EvenIndexArr(int[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i += 2)
+            {
+                for (int j = 0; j < arr.GetLength(1); j += 2)
+                {
+                    arr[i, j] *= arr[i, j];
+                }
+            }
+        }
+
+        private static void PrintArray(int[,] arr)
+        {
+            Console.WriteLine("\nМассив: \n" +
+                              "  |  0   1   2   3\n" +
+                              "------------------");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                Console.Write($"{i} |");
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i, j] < 10 && arr[i, j] >= 0) Console.Write($"  {arr[i, j]} ");
+                    else if (arr[i, j] < -9 || arr[i, j] > 99) Console.Write($"{arr[i, j]} ");
+                    else Console.Write($" {arr[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void FillArray(int[,] arr)
+        {
+            Random random = new Random();
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = random.Next(-9, 10);
+                }
+            }
         }
     }
 }
