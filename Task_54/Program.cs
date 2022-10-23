bc766b54ea5fc9e0762f3a19562ec89fcae9d8d3
Konstantsin <Drakula3D@mail.ru>
2022-10-23 20:15:19 +0300

    add Task_54

--- Task_54/Program.cs
+++ Task_54/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Task_54
+﻿using System;
+using System.Globalization;
+
+namespace Task_54
 {
     internal class Program
     {
@@ -19,6 +22,99 @@
             /*Задача 54.1: Задайте двумерный массив. Напишите программу, которая
                            упорядочит элементы по убыванию (или возрастанию).*/
             
+            /*Console.Write("Введите размер массива m x n:\n" +
+                          "m: ");
+            int numArrM = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("n: ");
+            int numArrN = Convert.ToInt32(Console.ReadLine());*/
+            int numArrM = 5;
+            int numArrN = 5;
+            int[,] arr = new int[numArrM, numArrN];
+
+
+            FillArray(arr);
+            PrintArray(arr);
+            
+            Console.WriteLine("Сортировка по строкам: ");
+            ArrangRowsArr(arr);
+            PrintArray(arr);
+            
+            Console.WriteLine("Сортировка всего массива от большево к меньшему: ");
+            ArrangArr(arr);
+            PrintArray(arr);
+        }
+
+        private static void ArrangArr(int[,] arr)
+        {
+            int count = 0;
+            int[] arr2 = new int[arr.GetLength(0) * arr.GetLength(1)];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr2[count] = arr[i, j];
+                    count++;
+                }
+            }
+            Array.Sort(arr2);
+            Array.Reverse(arr2);
+            count = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = arr2[count];
+                    count++;
+                }
+            }
+        }
+
+        private static void ArrangRowsArr(int[,] arr)
+        { 
+            int[] arr2 = new int[arr.GetLength(1)];
+            
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr2[j] = arr[i, j];
+                }
+                Array.Sort(arr2);
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = arr2[j];
+                }
+            }
+        }
+
+        private static void FillArray(int[,] arr)
+        {
+            Random random = new Random();
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = random.Next(-99, 100);
+                }
+            }
+        }
+        
+        private static void PrintArray(int[,] arr)
+        {
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    var lenght = arr[i, j].ToString(CultureInfo.InvariantCulture).Length;
+                    //редактор рекомендует использовать var и CultureInfo.InvariantCulture!
+                    if (lenght == 1)      Console.Write($"   {arr[i, j]}");
+                    else if (lenght == 2) Console.Write($"  {arr[i, j]}");
+                    else                  Console.Write($" {arr[i, j]}");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
