4bb3dea2757023a15a491cd5a700df493a91d739
Konstantsin <Drakula3D@mail.ru>
2022-10-23 20:56:17 +0300

    add Task_56

--- Task_56/Program.cs
+++ Task_56/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Task_56
+﻿using System;
+using System.Globalization;
+
+namespace Task_56
 {
     internal class Program
     {
@@ -16,6 +19,72 @@
                          Программа считает сумму элементов в каждой строке и выдаёт номер строки
                          с наименьшей суммой элементов: 1 строка*/
             
+            Console.Write("Введите размер массива m x n:\n" +
+                          "m: ");
+            int numArrM = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("n: ");
+            int numArrN = Convert.ToInt32(Console.ReadLine());
+            int[,] arr = new int[numArrM, numArrN];
+            
+            FillArray(arr);
+            PrintArray(arr);
+            EachColumnArr(arr);
+        }
+        
+        private static void EachColumnArr(int[,] arr)
+        {
+            int numberStr = 0;
+            int number = 0;
+            
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                number += arr[0, j];
+            }
+            
+            for (int i = 1; i < arr.GetLength(0); i++)
+            {
+                int count = 0;
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    count += arr[i, j];
+                }
+                if (number >= count)
+                {
+                    number = count;
+                    numberStr = i;
+                }
+            }
+            Console.WriteLine($"{numberStr + 1} Строка имеет наимешьшую сумму");
+        }
+        private static void FillArray(int[,] arr)
+        {
+            Random random = new Random();
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = random.Next(-10, 21);
+                }
+            }
+        }
+        
+        private static void PrintArray(int[,] arr)
+        {
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                Console.Write($"Строка {i + 1}: ");
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    var lenght = arr[i, j].ToString(CultureInfo.InvariantCulture).Length;
+                    //редактор рекомендует использовать var и CultureInfo.InvariantCulture!
+                    if (lenght == 1)      Console.Write($"   {arr[i, j]}");
+                    else if (lenght == 2) Console.Write($"  {arr[i, j]}");
+                    else                  Console.Write($" {arr[i, j]}");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
