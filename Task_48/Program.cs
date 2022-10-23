4ecf143ce9df0ae79e3334ad68b60ff0c4209ee5
Konstantsin <Drakula3D@mail.ru>
2022-10-23 14:44:11 +0300

    add Task_48

--- Task_48/Program.cs
+++ Task_48/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Task_48
+﻿using System;
+
+namespace Task_48
 {
     internal class Program
     {
@@ -12,6 +14,38 @@
                          1 2 3 4
                          2 3 4 5*/
             
+            int numArrM = 3;
+            int numArrN = 4;
+
+            int[,] arr = new int[numArrM, numArrN];
+
+            FillArray(arr);
+            PrintArray(arr);
+        }
+        private static void PrintArray(int[,] arr)
+        {
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i, j] < 10 && arr[i, j] >= 0) Console.Write($"  {arr[i, j]} ");
+                    else if (arr[i, j] < -9 || arr[i, j] > 99) Console.Write($"{arr[i, j]} ");
+                    else Console.Write($" {arr[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void FillArray(int[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = i + j;
+                }
+            }
         }
     }
 }
