bae8323b684ba2fbf8eeea1b55388a5a6424249e
Konstantsin <Drakula3D@mail.ru>
2022-11-13 19:08:21 +0300

    add Task_58

--- Task_58/Program.cs
+++ Task_58/Program.cs
@@ -19,47 +19,45 @@ namespace Task_58
                          С
                          7 13
                          14 25*/
-            
-            /*Console.Write("Введите размер массива m x n:\n" +
-                          "m: ");
-            int numArrM = Convert.ToInt32(Console.ReadLine());
-            
-            Console.Write("n: ");
-            int numArrN = Convert.ToInt32(Console.ReadLine());*/
 
-            int numM1 = 2;
-            int numN1 = 2;
-            int numM2 = 2;
-            int numN2 = 2;
-            int numFinal = 0;
-            if (numM1 == numN2) numFinal = numM1;
-            else if (numM2 == numN1) numFinal = numM2;
-            else Console.WriteLine("Матрицы нельзя перемножить");
-            int[,] matrixA = new int[numM1, numN1];
-            int[,] matrixB = new int[numM2, numN2];
-            int[,] matrixC = new int[numFinal, numFinal];
+            Console.Write("Введите размер матриц m x n:\n" +
+                          "количество строк 1й матрц m: ");
+            int numM1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("количество столбцов 1й матрц и строк 2й матрц: ");
+            int numN1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("количество столбцов 2й матрц: ");
+            int numN2 = Convert.ToInt32(Console.ReadLine());
+            
+            int numFinal;
+            numFinal = numM1;
+            if (numN2 > numFinal) numFinal = numN2;
+            
+            int[,] matrixA = new int[numM1, numN1];//[numM1, numN1]
             FillArray(matrixA);
             PrintArray(matrixA, "A");
+            int[,] matrixB = new int[numN1, numN2]; //new int[numM2, numN2]
             FillArray(matrixB);
             PrintArray(matrixB, "B");
+            int[,] matrixC = new int[numN1, numFinal];
             MultMatrix(matrixA, matrixB, matrixC);
             PrintArray(matrixC, "C");
         }
-
+        
         private static void MultMatrix(int[,] matrixA, int[,] matrixB, int[,] matrixC)
         {
-            matrixC[0, 0] = matrixA[0,0] * matrixB[0,0] + matrixA[0, 1] * matrixB[1, 0];
-            matrixC[0, 1] = matrixA[0,0] * matrixB[0,1] + matrixA[0, 1] * matrixB[1, 1];
-            matrixC[1, 0] = matrixA[1,0] * matrixB[0,0] + matrixA[1, 1] * matrixB[1, 0];
-            matrixC[1, 1] = matrixA[1,0] * matrixB[0,1] + matrixA[0, 1] * matrixB[1, 1];
-            PrintArray(matrixC, "C");
             for (int i = 0; i < matrixC.GetLength(0); i++)
             {
                 for (int j = 0; j < matrixC.GetLength(1); j++)
                 {
-                    matrixC[i, j] = matrixA[0,0] * matrixB[0,0] + matrixA[0, 1] * matrixB[1, 0];
+                    for (int k = 0; k < matrixA.GetLength(1); k++)
+                    { 
+                        matrixC[i,j] += matrixA[i,k] * matrixB[k,j];
+                    }
                 }
             }
+            
         }
 
         private static void FillArray(int[,] arr)
@@ -69,11 +67,11 @@ namespace Task_58
             {
                 for (int j = 0; j < arr.GetLength(1); j++)
                 {
-                    arr[i, j] = random.Next(-9, 10);
+                    arr[i, j] = random.Next(-3, 7);
                 }
             }
         }
-        
+
         private static void PrintArray(int[,] arr, string name)
         {
             Console.WriteLine($"Матрица {name}: ");
@@ -82,11 +80,11 @@ namespace Task_58
                 for (int j = 0; j < arr.GetLength(1); j++)
                 {
                     var lenght = arr[i, j].ToString(CultureInfo.InvariantCulture).Length;
-                    //редактор рекомендует использовать var и CultureInfo.InvariantCulture!
-                    if (lenght == 1)      Console.Write($"   {arr[i, j]}");
+                    if (lenght == 1) Console.Write($"   {arr[i, j]}");
                     else if (lenght == 2) Console.Write($"  {arr[i, j]}");
-                    else                  Console.Write($" {arr[i, j]}");
+                    else Console.Write($" {arr[i, j]}");
                 }
+
                 Console.WriteLine();
             }
         }
