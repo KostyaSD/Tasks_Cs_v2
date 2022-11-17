06b89dfa4d183e2630be4019bb74499b8fd6b70a
Konstantsin <Drakula3D@mail.ru>
2022-11-17 15:43:56 +0300

    add Task_64

--- Task_64/Program.cs
+++ Task_64/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace Task_64
+﻿using System;
+
+namespace Task_64
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            /*Задача 64:  Задайте значение N. Напишите программу, которая выведет все
+                          натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+                          N = 5 -> "5, 4, 3, 2, 1"
+                          N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+            
+            Console.WriteLine("Введите натуральное число больше 1:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            PrintNaturNum(number);
+        }
+
+        private static void PrintNaturNum(int number)
+        {
+            if (number < 0) Console.Write($"{number} не натуральное число");
+            if (number == 0) return;
+            Console.Write($"{number} ");
+            PrintNaturNum(number - 1);
         }
     }
 }
